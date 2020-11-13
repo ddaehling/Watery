e69846002beb4a19fb4d352dd99b6ff48ef446e7
ddaehling <57818464+ddaehling@users.noreply.github.com>
2020-11-13 22:06:31 +0100

    Initial Commit

---
+++ Watery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Watery
+//
+//  Created by Daniel Dähling on 13.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Watery/WateryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WateryApp.swift
+//  Watery
+//
+//  Created by Daniel Dähling on 13.11.20.
+//
+
+import SwiftUI
+
+@main
+struct WateryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
